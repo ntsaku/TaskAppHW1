01636339f7a06a10f0d37dc4e96bd3cb75c200a0
ntsaku <misc.sakurai@gmail.com>
2016-08-15 21:11:26 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/naoto/sakurai/taskapp/MainActivity.java
@@ -0,0 +1,178 @@
+package jp.techacademy.naoto.sakurai.taskapp;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import io.realm.Realm;
+import io.realm.RealmChangeListener;
+import io.realm.RealmResults;
+import io.realm.Sort;
+
+public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_TASK = "jp.techacademy.naoto.sakurai.taskapp.TASK";
+
+    private Realm mRealm;
+    private RealmResults<Task> mTaskRealmResults;
+    private RealmChangeListener mRealmListener = new RealmChangeListener() {
+        @Override
+        public void onChange() {
+            reloadListView();
+        }
+    };
+    private ListView mListView;
+    private TaskAdapter mTaskAdapter;
+    private EditText mCSearchText;
+    private Button mCSearchButton;
+
+//    カテゴリー検索の実行ボタンを押されたとき：
+//    mTaskRealmResults に検索結果を設定して, dateでソートしたあと, reloadListViewメソッドをよびリストを再表示する。
+
+    private View.OnClickListener mOnCSearchClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            mCSearchText = (EditText)findViewById(R.id.category_search_text);
+            String cond = mCSearchText.getText().toString();
+            if (cond.isEmpty()) {  /*検索条件が空なら全検索*/
+                mTaskRealmResults = mRealm.where(Task.class).findAll();
+            } else {
+                mTaskRealmResults = mRealm.where(Task.class).contains("category", cond).findAll();
+            }
+            mTaskRealmResults.sort("date", Sort.DESCENDING);
+            reloadListView();
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, InputActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Realmの設定
+        mRealm = Realm.getDefaultInstance();
+        mTaskRealmResults = mRealm.where(Task.class).findAll();
+        mTaskRealmResults.sort("date", Sort.DESCENDING);
+        mRealm.addChangeListener(mRealmListener);
+
+        //カテゴリ検索ボタンの設定
+        mCSearchButton = (Button)findViewById(R.id.category_search_button);
+        mCSearchButton.setOnClickListener(mOnCSearchClickListener);
+
+        // ListViewの設定
+        mTaskAdapter = new TaskAdapter(MainActivity.this);
+        mListView = (ListView) findViewById(R.id.listView1);
+
+        // ListViewをタップしたときの処理
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // 入力・編集する画面に遷移させる
+                Task task = (Task) parent.getAdapter().getItem(position);
+
+                Intent intent = new Intent(MainActivity.this, InputActivity.class);
+                intent.putExtra(EXTRA_TASK, task);
+
+                startActivity(intent);
+            }
+        });
+
+
+
+        // ListViewを長押ししたときの処理
+        mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // タスクを削除する
+
+                final Task task = (Task) parent.getAdapter().getItem(position);
+
+                // ダイアログを表示する
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                builder.setTitle("削除");
+                builder.setMessage(task.getTitle() + "を削除しますか");
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        RealmResults<Task> results = mRealm.where(Task.class).equalTo("id", task.getId()).findAll();
+
+                        mRealm.beginTransaction();
+                        results.clear();
+                        mRealm.commitTransaction();
+
+                        Intent resultIntent = new Intent(getApplicationContext(), TaskAlarmReceiver.class);
+                        PendingIntent resultPendingIntent = PendingIntent.getBroadcast(
+                                MainActivity.this,
+                                task.getId(),
+                                resultIntent,
+                                PendingIntent.FLAG_UPDATE_CURRENT
+                        );
+
+                        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+                        alarmManager.cancel(resultPendingIntent);
+
+                        reloadListView();
+                    }
+                });
+                builder.setNegativeButton("CANCEL", null);
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+                return true;
+            }
+        });
+
+        reloadListView();
+    }
+
+    private void reloadListView() {
+
+        ArrayList<Task> taskArrayList = new ArrayList<>();
+
+        for (int i = 0; i < mTaskRealmResults.size(); i++) {
+            Task task = new Task();
+
+            task.setId(mTaskRealmResults.get(i).getId());
+            task.setTitle(mTaskRealmResults.get(i).getTitle());
+            task.setContents(mTaskRealmResults.get(i).getContents());
+            task.setCategory(mTaskRealmResults.get(i).getCategory());
+            task.setDate(mTaskRealmResults.get(i).getDate());
+
+            taskArrayList.add(task);
+        }
+
+        mTaskAdapter.setTaskArrayList(taskArrayList);
+        mListView.setAdapter(mTaskAdapter);
+        mTaskAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        mRealm.close();
+    }
+}
